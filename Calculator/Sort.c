d423df030c2cb436434ebfe43fbfdb4557905b9b
Aadit Sharma <110082421+Aadit-AI@users.noreply.github.com>
2022-07-27 23:53:45 +0530

    Adding merge sort

--- Calculator/Sort.c
+++ Calculator/Sort.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+void mergesort(int a[],int l,int r);
+void merge(int a[],int l1,int r1,int l2,int r2);
+
 int main()
  {
+    
     int n,method,element,position;
     printf("Please enter the number of numbers in the array you want to sort:- ");
     scanf("%d",&n);
@@ -10,7 +15,7 @@ int main()
         scanf(" %d",&arr[i]);
     }
 
-     printf("Which sorting method you want to use :\nPress 1 for insertion sort\nPress 2 for bubble sort\nPress 3 for selection sort\n");
+    printf("Which sorting method you want to use :\nPress 1 for insertion sort\nPress 2 for bubble sort\nPress 3 for selection sort\nPress 4 for merge sort\n");
     scanf("%d",&method);
     switch(method){
     case 1:
@@ -67,5 +72,51 @@ int main()
         printf(",");
     }
   } break;
+  case 4:
+    mergesort(arr,0,n-1);
+    printf("\nMerge Sort Array result is : ");
+    for(int i = 0; i < n; i++) {
+    printf(" %d", arr[i]);
+    if (i!=n-1){
+        printf(",");
+    }
+  }
+    break;
+    
+  default:
+    printf("You have not chosen appropriate sorting method");
+    break;
+  }
+    return 0;
  }
+ void mergesort(int a[],int l,int r) {
+ int mid;
+ if(l<r) {
+   mid=(l+r)/2;
+   mergesort(a,l,mid); //left recursion
+   mergesort(a,mid+1,r); //right recursion
+   merge(a,l,mid,mid+1,r); //merging of two sorted sub-arrays
  }
+}
+void merge(int a[],int l1,int r1,int l2,int r2) {
+ int temp[50]; //array used for merging
+ int l,r,k;
+ l=l1; 
+ r=l2; 
+ k=0;
+ while(l<=r1 && r<=r2){ 
+   if(a[l]<a[r])
+      temp[k++]=a[l++];
+   else
+      temp[k++]=a[r++];
+}
+ while(l<=r1)
+   temp[k++]=a[l++];
+   while(r<=r2)
+     temp[k++]=a[r++];
+     for(l=l1,r=0;l<=r2;l++,r++)
+       a[l]=temp[r];
+       
+ 
+}
+
