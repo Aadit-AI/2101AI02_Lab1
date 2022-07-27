e0289ca2ad724de12f42b6dbea87cb6f6079495e
Aadit Sharma <110082421+Aadit-AI@users.noreply.github.com>
2022-07-27 23:44:47 +0530

    Adding insertion sort

--- Calculator/Sort.c
+++ Calculator/Sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
  {
-    int n;
+    int n,method,element;
     printf("Please enter the number of numbers in the array you want to sort:- ");
     scanf("%d",&n);
     int arr[n];
@@ -9,5 +9,26 @@ int main()
     for(int i=0;i<n;i++){
         scanf(" %d",&arr[i]);
     }
+
+     printf("Which sorting method you want to use :\nPress 1 for insertion sort\n");
+    scanf("%d",&method);
+    switch(method){
+    case 1:
+    for(int i = 1; i <= n - 1; i++) {
+    for(int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+     element = arr[j];
+     arr[j] = arr[j - 1];
+     arr[j - 1] = element;
+   }
+} 
+   printf("\nInsertion Sort Result is ");
+   for(int i = 0; i < n; i++) {
+    printf(" %d", arr[i]);
+    if (i!=n-1){
+        printf(",");
+    }
+  } 
+  break;
    
+}
 }
