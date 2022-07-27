879214674218c5f48834188068481ab477d19066
Aadit Sharma <110082421+Aadit-AI@users.noreply.github.com>
2022-07-27 22:42:22 +0530

    Adding sum operator

--- Calculator/1.c
+++ Calculator/1.c
@@ -6,7 +6,14 @@ int main(){
     printf("Enter your 2 numbers on which operations are to be performed");  
     scanf("%d %d",&n,&m);
     
-    printf("Enter the operation to be performed which are  + ; - ; * ; / ")
+    printf("Enter the operation to be performed which are  + ; - ; * ; / ");
     scanf(" %c", &operator);
+    switch (operator){
+    case '+' :
+            printf("%d",n+m);
+            break;
+    default : 
+            printf("Enter suitable operator");
+    }
     return 0;
 }
